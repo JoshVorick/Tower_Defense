3223ade17fca991791ef89fcbe677bc39acfd729
JoshVorick <josh.vorick@gmail.com>
2014-06-29 16:57:23 -0500

    Enemies sort properly

--- src/Game/Enemies/enemies.c
+++ src/Game/Enemies/enemies.c
@@ -32,6 +32,74 @@ void initEnemy(Enemy *enemy, int type, Grid_Tile *startGrid, Sprite sprites[]){
   }
 }
 
+Enemy* msortEnemies(Enemy *head){
+  if(head == NULL)
+    return NULL;
+
+  Enemy *p, *q, *e, *tail;
+  int insize, nmerges, psize, qsize, i;
+
+  insize = 1;
+  
+  while(TRUE){
+    p = head;
+    head = NULL;
+    tail = NULL;
+
+    nmerges = 0;//num merges this pass
+
+    while(p){
+      nmerges++;//There is a merge that needs doing
+      
+      q = p;
+      psize = 0;
+      for(i=0; i < insize; i++){
+        psize++;
+        q = q->nextEnemy;
+        if(q== NULL) 
+          break;
+      }
+
+      qsize = insize;
+
+      while(psize > 0 || (qsize > 0 && q)){
+        //Decide if next enemy is in p or q
+        if(psize == 0){
+          e = q;
+          q = q->nextEnemy;
+          qsize--;
+        }else if(qsize == 0 || q == NULL){
+          e = p;
+          p = p->nextEnemy;
+          psize--;
+        }else if(p->myGrid->distFromExit < q->myGrid->distFromExit || (p->myGrid->distFromExit == q->myGrid->distFromExit && p->y > q->y)){
+          e = p;
+          p = p->nextEnemy;
+          psize--;
+        }else{
+          e = q;
+          q = q->nextEnemy;
+          qsize--;
+        }
+
+        if(tail)
+          tail->nextEnemy = e;
+        else
+          head = e;
+
+        tail = e;
+      }
+      p = q;
+    }
+    tail->nextEnemy = NULL;
+    
+    if(nmerges <= 1)
+      return head;
+
+    insize *= 2;
+  }
+};
+
 void updateEnemies(Game *game){
   Enemy *curEnemy = game->enemies;
   Enemy *prevEnemy = NULL;
@@ -98,72 +166,6 @@ void updateEnemies(Game *game){
       curEnemy = curEnemy->nextEnemy;
     }
   }
+  game->enemies = msortEnemies(game->enemies);
 };
 
-Enemy* msortEnemies(Enemy *head){
-  if(!head)
-    return NULL;
-
-  Enemy *p, *q, *e, *tail;
-  int insize, nmerges, psize, qsize, i;
-
-  insize = 1;
-  
-  while(TRUE){
-    p = head;
-    head = NULL;
-    tail = NULL;
-
-    nmerges = 0;//num merges this pass
-
-    while(p){
-      nmerges++;//There is a merge that needs doing
-      
-      q = p;
-      psize = 0;
-      for(i=0; i < insize; i++){
-        psize++;
-        q = q->nextEnemy;
-        if(!q) 
-          break;
-      }
-
-      qsize = insize;
-
-      while(psize > 0 || (qsize > 0 && q)){
-        //Decide if next enemy is in p or q
-        if(psize == 0){
-          e = q;
-          q = q->nextEnemy;
-          qsize--;
-        }else if(qsize == 0 || !q){
-          e = p;
-          p = p->nextEnemy;
-          psize--;
-        }else if(p->myGrid->distFromExit < q->myGrid->distFromExit || p->y > q->y || p->x > q->x){
-          e = p;
-          p = p->nextEnemy;
-          psize--;
-        }else{
-          e = q;
-          q = q->nextEnemy;
-          qsize--;
-        }
-
-        if(tail)
-          tail->nextEnemy = e;
-        else
-          head = e;
-
-        tail = e;
-      }
-      p = q;
-    }
-    tail->nextEnemy = NULL;
-    
-    if(nmerges <= 1)
-      return head;
-
-    insize *= 2;
-  }
-};
